09c94e3fc7de1d07ee3a65886bdc9635e9209ce8
Cxc <393171302@qq.com>
2017-01-17 14:15:32 +0800

    Update decision_tree.h

--- Decision-Tree/decision_tree.h
+++ Decision-Tree/decision_tree.h
@@ -31,26 +31,26 @@ public:
 	CART() = default; 
 	~CART(); 
 
-	inline void SetTrainingSetFilePath(const std::string &filepath) throw(); //设置训练样本文件路径
-	std::vector<training_data> GetTrainingSetFromFile(const std::string filename) const throw(); // 从本地文件中读取训练集
+	explicit CART(const CART &) = delete;
+	CART &operator =(const CART &) = delete;
+	
+	inline void SetTrainingSetFilePath(const std::string &filepath) noexcept; //设置训练样本文件路径
+	std::vector<training_data> GetTrainingSetFromFile(const std::string filename) const noexcept; // 从本地文件中读取训练集
 	bool FeedWithTrainingSet() throw(); // 输入训练集
-	bool SaveTreeToFile(const std::string &filename) const throw(); // 保存决策树结构和参数至本地
-	bool LoadTreeFromFile(const std::string &filename) throw(); // 从本地读取决策树结构和参数
+	bool SaveTreeToFile(const std::string &filename) const noexcept; // 保存决策树结构和参数至本地
+	bool LoadTreeFromFile(const std::string &filename) noexcept; // 从本地读取决策树结构和参数
 
 private:
-
-	explicit CART(const CART &cart_tree);
-	CART &operator =(const CART &cart_tree);
 	
 	int GetMaxLabel(const std::vector<training_data> &training_set) const throw(std::logic_error); // 获取训练集中样本数最多的类别
-	bool isSameAttr(const std::vector<training_data> &training_set) const throw(); // 判断样本在属性集上是否取值一致
-	bool isSameLabel(const std::vector<training_data> &training_set) const throw(); // 判断训练集中类别标签是否一致
+	bool isSameAttr(const std::vector<training_data> &training_set) const noexcept; // 判断样本在属性集上是否取值一致
+	bool isSameLabel(const std::vector<training_data> &training_set) const noexcept; // 判断训练集中类别标签是否一致
 	bool isAttrEmpty(const std::vector<int> &attr) const throw(); // 判断属性集是否为空
 	float Gini(const std::vector<training_data> &training_set) const throw();// 求基尼值
 	float GetGini_Index(const std::vector<training_data> &training_set,const int &attr_type) const throw(std::logic_error); // 求基尼指数
 	int GetPatitionAttr(const std::vector<training_data> &training_set,const std::vector<int> &attr_set) const throw(std::logic_error); // 求最佳划分参数
-	dt_tree TreeGenerate(const std::vector<training_data> &training_set,std::vector<int> &attr_set) throw(); // 生成决策树结点
-	void DeleteNode(dt_tree node) throw(); // 删除节点
+	dt_tree TreeGenerate(const std::vector<training_data> &training_set,std::vector<int> &attr_set) noexcept; // 生成决策树结点
+	void DeleteNode(dt_tree node) noexcept; // 删除节点
 
 };
 
